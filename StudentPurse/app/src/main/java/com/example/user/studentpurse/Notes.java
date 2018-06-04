100f9a14d5056788ac148095a39f0977ef555770
f0xx1996 <36085200+f0xx1996@users.noreply.github.com>
2018-06-04 07:28:38 +0400

    First edit
    Изменил ссылки вызовоз активити

--- StudentPurse/app/src/main/java/com/example/user/studentpurse/Notes.java
+++ StudentPurse/app/src/main/java/com/example/user/studentpurse/Notes.java
@@ -22,7 +22,7 @@ public class Notes extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                Intent intent = new Intent(getBaseContext(), SplashScreen.class);
                 startActivity(intent);
                 }
         });
