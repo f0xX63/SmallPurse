8ebf1018e4399b39862a4b879d49b14f84ad6542
f0xx1996 <36085200+f0xx1996@users.noreply.github.com>
2018-06-04 11:56:43 +0400

    SmallPurse
    Добавление категорий и выбор подкатегорий в зависимости от категории

--- StudentPurse/app/src/main/java/com/example/user/studentpurse/Plus.java
+++ StudentPurse/app/src/main/java/com/example/user/studentpurse/Plus.java
@@ -1,13 +1,20 @@
 package com.example.user.studentpurse;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.user.studentpurse.Domain.Categories;
+import com.example.user.studentpurse.Domain.SmallPurseParameters;
+import com.example.user.studentpurse.WorkOfFile.JSONHelper;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -17,11 +24,17 @@ public class Plus extends AppCompatActivity {
     Spinner Date;
     Spinner Category;
     Spinner Place;
+    SmallPurseParameters parameters;
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minus);
-
+        try {
+            parameters = JSONHelper.importFromJSON(Plus.this);
+        } catch (IOException e) {
+            Toast.makeText(Plus.this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
         Moneys = (Spinner)findViewById(R.id.moneys);
         Date = (Spinner)findViewById(R.id.date);
         Category = (Spinner) findViewById(R.id.category);
@@ -66,20 +79,30 @@ public class Plus extends AppCompatActivity {
 
             }
         });
-
-        final List<String> CategoryList = new ArrayList<String>();
-        CategoryList.add("Еда");
-        CategoryList.add("Одежда");
-        CategoryList.add("Проезд");
-        CategoryList.add("Добавить");
+        List<String> categories = new ArrayList<String>();
+        if(parameters!=null) {
+            for (Categories category : parameters.categories) {
+                categories.add(category.Name);
+            }
+        }
+        final List<String> CategoryList = categories;
 
         ArrayAdapter<String> CategoryAdapter = new ArrayAdapter<>(Plus.this,R.layout.support_simple_spinner_dropdown_item,CategoryList);
         Category.setAdapter(CategoryAdapter);
-
+        final List<String> subCategories = new ArrayList<String>();
         Category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(Plus.this, CategoryList.get(position), Toast.LENGTH_SHORT).show();
+                for (Categories category: parameters.categories) {
+                    if(category.Name == CategoryList.get(position)){
+                        subCategories.clear();
+                        for (String subCategory: category.SubCategories) {
+                            subCategories.add(subCategory);
+                        }
+                        break;
+                    }
+                }
             }
 
             @Override
@@ -89,11 +112,8 @@ public class Plus extends AppCompatActivity {
         });
 
 
-        final List<String> PlaceList = new ArrayList<String>();
-        PlaceList.add("Перчини");
-        PlaceList.add("Спортмастер");
-        PlaceList.add("Ашан");
-        PlaceList.add("Добавить");
+
+        final List<String> PlaceList = subCategories;
 
         ArrayAdapter<String> PlaceAdapter = new ArrayAdapter<>(Plus.this,R.layout.support_simple_spinner_dropdown_item,PlaceList);
         Place.setAdapter(PlaceAdapter);
